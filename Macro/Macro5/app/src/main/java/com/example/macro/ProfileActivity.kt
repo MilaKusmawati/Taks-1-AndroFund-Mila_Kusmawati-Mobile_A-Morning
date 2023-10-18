17cd39399c802caaf79581e1d178023fcb8e07eb
MilaKusmawati <milakusmawati05@gmail.com>
2023-10-18 23:11:25 +0700

    mila

---
+++ Macro/Macro5/app/src/main/java/com/example/macro/ProfileActivity.kt
@@ -0,0 +1,40 @@
+package com.example.macro
+
+import android.content.Intent
+import android.os.Bundle
+import android.window.SplashScreen
+import androidx.appcompat.app.AppCompatActivity
+import com.example.macro.databinding.ActivityMainBinding
+import com.example.macro.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.bararticle.setOnClickListener {
+            startActivity(Intent(this, ArticleActivity::class.java))
+        }
+        binding.barhome.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+        binding.btnEditProfile.setOnClickListener {
+            startActivity(Intent(this, EditProfileActivity::class.java))
+        }
+        binding.btnChangePassword.setOnClickListener {
+            startActivity(Intent(this, ChangePasswordActivity::class.java))
+        }
+        binding.btnCustomerService.setOnClickListener {
+            startActivity(Intent(this, CustomerServiceActivity::class.java))
+        }
+        binding.btnFaq.setOnClickListener {
+            startActivity(Intent(this, FaqActivity::class.java))
+        }
+    }
+}
